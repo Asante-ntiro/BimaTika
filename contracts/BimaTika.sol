52bb520f83d81003e4bdfbe62e49e4f500a305c7
Asante-ntiro <asante.ntiro@gmail.com>
2023-02-28 00:18:50 +0300

    updated contracts

--- contracts/BimaTika.sol
+++ contracts/BimaTika.sol
@@ -1,65 +1,61 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-// import "./BimaCoin.sol"
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "./BimaCoin.sol";
 
 contract HealthInsurance is ReentrancyGuard{
+
+    using Address for address;
+
+    IERC20 public immutable bimaCoin;
     
+    constructor (address tokenAddress) {
+        bimaCoin = IERC20(tokenAddress);
+    }
     
-    /**
-     * we can choose 1 of 2 implementations.. 
-     * Using a stablecoin provided by FileCoin that is tagged to the USDC and FIL called oneFIL
-     * or using our own token, BimaCoin...
-     */
+    address public merchant;
+    address public user;
+
+    uint256 public exchangeRate;
+    
+    uint256 private constant FIXED_FEE = 1;
 
 
-
-    //using oneFIL
-    address public constant oneFIL = //oneFIL address
-
-    uint256 public fixedFee = 10;
 
     mapping(address => uint256) public tokenBalance;
 
     mapping(address => bool) public registeredMerchants;
-    
-    
-    
-    
-    
 
-
-
-    function registerMerchant(address merchant, uint256 exchangeRate) public returns (bool) {
+    function registerMerchant(address _merchant, uint256 _exchangeRate) public returns (bool) {                
+        require (registeredMerchants[_merchant] != registeredMerchants[msg.sender], "Merchant not registered!");
         
-        
-        registeredMerchants[merchant] = true;
+        exchangeRate = _exchangeRate;
+        return true;
     }
 
-
-
-    function makePurchase() public {
+    //vendor purchases tokens
+    function makePurchase(uint256 amount) public {
         require(registeredMerchants[msg.sender], "Merchant not registered");
-
-
-        tokenBalance[msg.sender] += msg.value / exchangeRate;
+        bimaCoin.transfer(msg.sender, amount);
     }
     
+    //user receives tokens from merchant
+    function receiveTokens(address customer, uint256 amount) public {
+        bimaCoin.transfer(customer, amount);
+        
+    }
 
-
-    
     function makeClaim() public {
         uint256 tokens = tokenBalance[msg.sender];
-
-
         require(tokens > 0, "No tokens available");
-
-
         tokenBalance[msg.sender] = 0;
 
+    }
 
-        // perform claim processing logic here
-        // payment to health care service provider
+    function fixedFee() external pure returns (uint256) {
+        return FIXED_FEE;
     }
 }
